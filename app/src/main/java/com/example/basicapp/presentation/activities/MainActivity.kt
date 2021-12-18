0db97421484c7fe5379fd5f48e8a8c9b6ae3ecfb
Arman Hossain <arman.hossain.bd93@gmail.com>
2021-12-18 14:00:24 +0600

    SplashScreen

--- app/src/main/java/com/example/basicapp/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/example/basicapp/presentation/activities/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.example.basicapp.presentation.activities
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import com.example.basicapp.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setBaseContentView(binding.root)
+
+        animateViews()
+    }
+
+    fun animateViews() {
+        ObjectAnimator
+            .ofFloat(binding.txtMain, "alpha", 0f, 1f)
+            .setDuration(1000L)
+            .start()
     }
 }
--- app/src/main/java/com/example/basicapp/presentation/activities/SplashActivity.kt
+++ app/src/main/java/com/example/basicapp/presentation/activities/SplashActivity.kt
@@ -1,17 +1,31 @@
 package com.example.basicapp.presentation.activities
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import com.example.basicapp.databinding.ActivitySplashBinding
-import com.example.basicapp.presentation.activities.BaseActivity
+import java.util.*
+import java.util.Timer
+import kotlin.concurrent.schedule
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : BaseActivity() {
 
     private lateinit var binding: ActivitySplashBinding
+    private var context: Context = this
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        Timer("startMain", false).schedule(2500) {
+            startActivity(Intent(context, MainActivity::class.java))
+            overridePendingTransition(0, 0)
+            finish()
+        }
+
     }
 
 }
